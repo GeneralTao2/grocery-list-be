e250b5a9feb40e9d00e10e9c48b38193210cdfa8
valeriuchicu <102046963+valeriuchicu@users.noreply.github.com>
2022-06-14 16:30:35 +0300

    INTPO002PD-1374 - Cart content 06-14-2022-16-30

--- src/test/java/com/grocery/shop/service/CartServiceImplTest.java
+++ src/test/java/com/grocery/shop/service/CartServiceImplTest.java
@@ -5,11 +5,18 @@ import com.grocery.shop.dto.CartItemDto;
 import com.grocery.shop.dto.UserDetailsImpl;
 import com.grocery.shop.exception.NotEnoughProductsInStockException;
 import com.grocery.shop.exception.ProductNotFoundException;
+import com.grocery.shop.exception.UserNotFoundException;
 import com.grocery.shop.mapper.ProductMapper;
-import com.grocery.shop.model.*;
+import com.grocery.shop.model.Cart;
+import com.grocery.shop.model.Product;
+import com.grocery.shop.model.ProductCategory;
+import com.grocery.shop.model.Role;
+import com.grocery.shop.model.Type;
+import com.grocery.shop.model.User;
 import com.grocery.shop.repository.CartRepository;
 import com.grocery.shop.repository.ProductRepository;
 import com.grocery.shop.repository.UserRepository;
+import com.grocery.shop.security.JwtTokenUtil;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -20,13 +27,15 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.HashMap;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -41,6 +50,9 @@ class CartServiceImplTest {
 
     @Mock
     private UserRepository userRepository;
+
+    @Mock
+    private JwtTokenUtil jwtTokenUtil;
 
     @InjectMocks
     private CartServiceImpl cartService;
@@ -174,5 +186,72 @@ class CartServiceImplTest {
                 .isEqualTo(570);
 
         SecurityContextHolder.clearContext();
+    }
+
+    @Test
+    void shouldGetCartItemListFromCart(){
+        final User user = new User("adamsmith@yahoo.com", "12345", Role.USER);
+        final Authentication authMock = mock(Authentication.class);
+        final SecurityContext securityContext = mock(SecurityContext.class);
+
+        final List<Product> productList = Arrays.asList(
+                new Product(1L, "image1", "name1", 50, 4, "Description1", 5, Type.WEIGHABLE, 1,
+                        ProductCategory.FRUITS, 1000),
+                new Product(2L, "image2", "name2", 40, 3, "Description2", 6, Type.WEIGHABLE, 1,
+                        ProductCategory.FRUITS, 1000)
+        );
+        final Map<Product, Integer> cartItems = new HashMap<>();
+        cartItems.put(productList.get(0), 5);
+        cartItems.put(productList.get(1), 8);
+        final Cart cart = new Cart(1L, new User(), cartItems);
+
+        final List<CartItemDto> expectedDtoList = Arrays.asList(
+                new CartItemDto(ProductMapper.MAPPER.toDTOShort(productList.get(0)), 5),
+                new CartItemDto(ProductMapper.MAPPER.toDTOShort(productList.get(1)), 8)
+        );
+
+        final Long productId1 = productList.get(0).getId();
+        final Long productId2 = productList.get(1).getId();
+       // =====================================================================
+
+        when(securityContext.getAuthentication()).thenReturn(authMock);
+        SecurityContextHolder.setContext(securityContext);
+
+        when(authMock.getPrincipal()).thenReturn(new UserDetailsImpl(user));
+
+        when(jwtTokenUtil.generateToken(user)).thenReturn("Adam's Token");
+        when(jwtTokenUtil.getUsernameFromToken("Adam's Token"))
+                .thenReturn("adamsmith@yahoo.com");
+
+
+        when(userRepository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
+
+
+//        doReturn(1L).when(
+//                userRepository.findByEmail(user.getEmail())
+//                .orElseThrow(UserNotFoundException::new))
+//                .getId();
+
+        when(userRepository.findByEmail(user.getEmail())
+                .orElseThrow(UserNotFoundException::new)
+                .getId())
+                .thenReturn(1L);
+
+
+        when(authMock.getPrincipal()).thenReturn(new UserDetailsImpl(user));
+
+
+
+        CartDto actualDto = cartService.getCartItems("Adam's Token");
+
+        assertThat(5).isEqualTo(5);
+
+        SecurityContextHolder.clearContext();
+
+    }
+
+    @Test
+    void shouldAddItemsToCard(){
+
     }
 }
